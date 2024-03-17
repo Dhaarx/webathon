7cf226b630fb71cdaca856cdf354de56704dd15d
Dhaarx <dharineesh.nm2022ai-ds@sece.ac.in>
2024-03-17 23:43:25 +0530

    some changes in the backend

--- auth-backend/index.js
+++ auth-backend/index.js
@@ -4,25 +4,31 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
 
-// Connect to MongoDB
+const corsOption = {
+  origin:['https://webathon-eta.vercel.app/','http://localhost:3000'],
+  credentials:true,
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(bodyParser.json())
 async function dbconnect(){
   await mongoose.connect('mongodb://localhost:27017/express');
 }
 
 dbconnect()
-// server.js
-// Define user schema
+
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
 });
+
 const User = mongoose.model('User', userSchema);
 
 // Signup route
